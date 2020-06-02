dc8be8202fb02f574deb7df37baa440c049cf523
Swapnil Masurekar <swapnil3597@gmail.com>
2020-06-02 09:54:00 +0530

    Create csv2tfrecord.py

---
+++ tfrecord_util/csv2tfrecord.py
@@ -0,0 +1,11 @@
+"""
+  Create tfrecords from csv
+  csv format:
+  
+  path,label
+  gs://<bucket-name>/path-to-img1.jpg,label1
+  gs://<bucket-name>/path-to-img2.jpg,label2
+  gs://<bucket-name>/path-to-img3.jpg,label3
+  gs://<bucket-name>/path-to-img4.jpg,label2
+  ...
+"""
